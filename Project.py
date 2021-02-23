57f8945a7a6ec912f8888536078393035f03a3bc
99003745 <ujjawal.kumar@ltts.com>
2021-02-23 22:39:03 +0530

    updated

--- Project.py
+++ Project.py
@@ -7,5 +7,10 @@ x=re.findall(find_word,  read_file, re.M|re.I)
 
 file_save= find_word+'.txt'
 write_file=open(file_save,'w+')
-write_file.write(str(x)+'\n')
-write_file.write('No. of time word repeated:'+ str(len(x)))
+#write_file.write(str(x)+'\n')
+write_file.write('No. of time word repeated:'+ str(len(x))+'\n')
+with open("input.txt") as openfile:
+    for line in openfile:
+        for part in line.split():
+            if find_word in part:
+                write_file.write(str(part)+'\n')
