0bf552dfa49ea92e5446a5cec8635ce50f800b2e
99003745 <ujjawal.kumar@ltts.com>
2021-02-23 17:27:46 +0530

    updated

--- Project.py
+++ Project.py
@@ -1,5 +1,11 @@
 import re
 
 input_file=open("input.txt", "r")
+read_file =input_file.read()
+find_word=input("ENTER THE WORD YOU WANT TO SEARCH: ")
+x=re.findall(find_word,  read_file, re.M|re.I)
 
-print(input_file.read)
+file_save= find_word+'.txt'
+write_file=open(file_save,'w+')
+write_file.write(str(x)+'\n')
+write_file.write('No. of time word repeated:'+ str(len(x)))
