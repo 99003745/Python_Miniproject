a2250fc7b3264c8487737605be312a57dbda4130
99003745 <ujjawal.kumar@ltts.com>
2021-02-25 18:32:25 +0530

    class added

--- Project.py
+++ Project.py
@@ -1,32 +1,36 @@
 import re
 
-count = 0
+class  python_miniproject() :
+    def __init__(self):
+        print("WELCOME TO WORD FINDER")
+        self.count = 0
+        self.input_file = open("input.txt", "r")
+        self.read_file = self.input_file.read()
 
 
-input_file = open("input.txt", "r")
+    def taking_input(self):
+        self.Taking_No_int = int(input("ENTER THE No. OF WORDS YOU WANT TO SEARCH: "))
+        for y in range(self.Taking_No_int):
+             find_word = input("Enter The Keyword: ")
+             x = re.findall(find_word,  self.read_file, re.M | re.I)
 
-read_file = input_file.read()
+             input_file1 = self.read_file.split()
 
+             file_save = find_word+'.txt'
+             write_file = open(file_save, 'w+')
+             write_file.write('No. of time ' + find_word + ' repeated:' + str(len(x))+'\n')
+             print("A NEW TEXT FILE " + find_word + " IS CREATING....")
 
-Taking_No_int = int(input("ENTER THE No. OF WORDS YOU WANT TO SEARCH: "))
+             for i in range(len(input_file1)):
+                word = re.match(find_word, input_file1[i], re.M | re.I)
+                if word:
+                    self.count += 1
 
-for y in range(Taking_No_int):
-    find_word = input("Enter The Keywods: ")
-    x = re.findall(find_word,  read_file, re.M | re.I)
+                    str1 = (input_file1[i-1]+' ' + input_file1[i] + ' '+input_file1[i+1])
+                    write_file.write(str(self.count) + ' :')
+                    write_file.writelines(str(str1) + '\n')
+                #close_file = self.input_file.close()
 
-    input_file1 = read_file.split()
+obj = python_miniproject()
 
-    file_save = find_word+'.txt'
-    write_file = open(file_save, 'w+')
-    write_file.write('No. of time ' + find_word + ' repeated:' + str(len(x))+'\n')
-
-    for i in range(len(input_file1)):
-        word = re.match(find_word, input_file1[i], re.M | re.I)
-        if word:
-             count += 1
-
-             str1 = (input_file1[i-1]+' ' + input_file1[i] + ' '+input_file1[i+1])
-             write_file.write(str(count) + ' :')
-             write_file.write(str(str1) + '\n')
-             #print("A NEW TEXT FILE " + find_word + " IS CREATED")
-close_file = input_file.close()
+obj.taking_input()
