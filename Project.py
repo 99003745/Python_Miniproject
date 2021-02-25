512f8d699aa365d73e615ffd6b6916fd66b77af7
99003745 <ujjawal.kumar@ltts.com>
2021-02-25 14:03:04 +0530

    updated32

--- Project.py
+++ Project.py
@@ -7,23 +7,26 @@ input_file = open("input.txt", "r")
 
 read_file = input_file.read()
 
-find_word = input("ENTER THE WORD YOU WANT TO SEARCH: ")
 
-x = re.findall(find_word,  read_file, re.M | re.I)
+Taking_No_int = int(input("ENTER THE No. OF WORDS YOU WANT TO SEARCH: "))
 
-input_file1 = read_file.split()
+for y in range(Taking_No_int):
+    find_word = input("Enter The Keywods: ")
+    x = re.findall(find_word,  read_file, re.M | re.I)
 
-file_save = find_word+'.txt'
-write_file = open(file_save, 'w+')
+    input_file1 = read_file.split()
 
-for i in range(len(input_file1)):
-    word = re.match(find_word, input_file1[i], re.M | re.I)
-    if word:
-        count += 1
+    file_save = find_word+'.txt'
+    write_file = open(file_save, 'w+')
+    write_file.write('No. of time ' + find_word + ' repeated:' + str(len(x))+'\n')
 
-        str1 = (input_file1[i-1]+' ' + input_file1[i] + ' '+input_file1[i+1])
-        write_file.write(str(str1) + '\n')
-        write_file.write(str(count) + ' :')
+    for i in range(len(input_file1)):
+        word = re.match(find_word, input_file1[i], re.M | re.I)
+        if word:
+             count += 1
 
-
-write_file.write('No. of time word repeated:' + str(len(x))+'\n')
+             str1 = (input_file1[i-1]+' ' + input_file1[i] + ' '+input_file1[i+1])
+             write_file.write(str(count) + ' :')
+             write_file.write(str(str1) + '\n')
+             #print("A NEW TEXT FILE " + find_word + " IS CREATED")
+close_file = input_file.close()
